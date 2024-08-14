a93c3505d157fc052832052e739c6fbffafbddf2
QKnot <rahulcse11220120819@gmail.com>
2024-08-15 01:27:18 +0600

    Modal Updated

--- script.js
+++ script.js
@@ -236,28 +236,6 @@ function shareLink() {
         });
     }
 
-
-    document.getElementById('shareTwitter').onclick = function() {
-        window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(shareableLink)}&text=Check out my cover page data!`, '_blank');
-    }
-
-    document.getElementById('shareFacebook').onclick = function() {
-        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareableLink)}`, '_blank');
-    }
-
-    document.getElementById('shareLinkedIn').onclick = function() {
-        window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(shareableLink)}&title=Cover Page Generator Data`, '_blank');
-    }
-
-
-    document.getElementById('shareDiscord').onclick = function() {
-
-        navigator.clipboard.writeText(shareableLink).then(() => {
-            alert('Link copied to clipboard! You can now paste it in Discord.');
-        });
-    }
-
-
     document.getElementById('shareMail').onclick = function() {
         const subject = encodeURIComponent("Cover Page Generator Data");
         const body = encodeURIComponent(`Check out my cover page data: ${shareableLink}`);
@@ -265,14 +243,3 @@ function shareLink() {
     }
 }
 
-
-document.getElementById('shareDiscord').onclick = function() {
-    navigator.clipboard.writeText(shareableLink).then(() => {
-        const button = document.getElementById('shareDiscord');
-        const originalText = button.innerHTML;
-        button.innerHTML = '<img src="path/to/check-icon.svg" alt="Success"><span>Copied!</span>';
-        setTimeout(() => {
-            button.innerHTML = originalText;
-        }, 2000);
-    });
-}
